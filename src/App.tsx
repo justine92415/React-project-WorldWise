e7b359c76bba846064f848750764e5904e09d9ad
justine92415 <justine92415@gmail.com>
2023-08-03 21:47:27 +0800

    210. Building the Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
-import Home from './pages/Home';
+import Home from './pages/HomePage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="product" element={<Product></Product>}></Route>
         <Route path="pricing" element={<Pricing></Pricing>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
         <Route path="app" element={<AppLayout></AppLayout>}></Route>
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
